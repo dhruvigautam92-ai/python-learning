2b32b72dbab24d7857c3b16950debe714044da8a
dhruvigautam92-ai <dhruvigautam92@gmail.com>
2026-02-11 10:15:54 +0530

    Add student class with initialization and examples

---
+++ oop/basics_oop.py
@@ -0,0 +1,11 @@
+class student: 
+    
+    def __init__(self,name): 
+        self.name = name  
+        print("adding new student in database")
+    
+    
+s1=student("dhruvi")
+print(s1.name)
+s2=student("bittu")
+print(s2.name)
